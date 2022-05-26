5c4c5c381f96b8c8e80b86f87229348e02c649c0
martialo12 <martialo218@gmail.com>
2022-05-26 15:59:11 +0200

    refactoring

--- backend/app/core/custom_lib/webhookaction.py
+++ backend/app/core/custom_lib/webhookaction.py
@@ -121,7 +121,8 @@ class WebhookAction:
             question = "J'espère que tu vas bien. Comment puis je t'aider?"
             res = self.ful.main_response(
                 fulfillment_text=self.ful.fulfillment_text(
-                    f"Bonjour tg://user?id=<{self.user_info['id']}>\n {question}"
+                    f"Bonjour *{self.user_info['given_name']}* \n {question}" +
+                    f" <a href='tg://user?id={self.user_info['id']}'>{self.user_info['given_name']}</a>"
                 ),
                 fulfillment_messages=None,
                 output_contexts=None,
